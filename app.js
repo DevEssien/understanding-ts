0db66c6beef0cc59a14a508001cb609c02c0074c
essien emmanuel <essienemma300@gmail.com>
2023-11-16 20:40:48 +0100

    enum type

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
+//ENUM TYPE
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: 'Essien',
+//     age: 30,
+//     hobbies: [ 'sports', 'fishing', 'gaming'],
+//     role: [2, 'author']
+// }
 var person = {
     name: 'Essien',
     age: 30,
-    hobbies: ['sports', 'fishing', 'gaming']
+    hobbies: ['sports', 'fishing', 'gaming'],
+    role: Role.ADMIN
 };
-var favActivities;
-favActivities = ['gaming'];
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
+if (person.role === Role.ADMIN)
+    console.log('is admin');
--- basics.ts
+++ basics.ts
@@ -2,9 +2,36 @@ function add(n1: number, n2: number) {
     return n1 + n2;
   }
   
-  const num1 = "5";
+  const num1 = 5;
   const num2 = 2.3;
   
   const result = add(num1, num2);
   console.log(result);
   
+  //ENUM TYPE
+
+enum Role { ADMIN, READ_ONLY, AUTHOR }
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: 'Essien',
+//     age: 30,
+//     hobbies: [ 'sports', 'fishing', 'gaming'],
+//     role: [2, 'author']
+// }
+
+const person = {
+    name: 'Essien',
+    age: 30,
+    hobbies: [ 'sports', 'fishing', 'gaming'],
+    role: Role.ADMIN
+}
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) console.log('is admin');
