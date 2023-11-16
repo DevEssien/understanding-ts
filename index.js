4dd3c12e4cfc7f7fb19076d4690914318f5b5448
essien emmanuel <essienemma300@gmail.com>
2023-11-16 11:12:17 +0100

    finished the initial setup

--- index.js
+++
@@ -1,9 +0,0 @@
-var input1 = document.querySelector('#input1');
-var input2 = document.querySelector('#input2');
-var button = document.querySelector('button');
-var add = function (num1, num2) {
-    return num1 + num2;
-};
-button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
-    console.log(add(+input1.value, +input2.value));
-});
