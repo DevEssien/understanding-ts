eaead46d6e54b02d446c63aeeec26fe080d91fbc
essien emmanuel <essienemma300@gmail.com>
2023-11-18 09:04:50 +0100

    working with classes, using private keyword

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,22 @@
 "use strict";
-const add = (...numbers) => {
-    return numbers.reduce((total, currentVal) => {
-        return total + currentVal;
-    }, 0);
-};
-console.log(add(1, 2, 3, 4, 5, 6));
+class Department {
+    constructor(n) {
+        this.employees = [];
+        this.name = n;
+    }
+    describe() {
+        console.log('This is the department of ', this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfo() {
+        console.log('employee length ', this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department('accounting');
+accounting.addEmployee('tony stark');
+accounting.addEmployee('Jeff Cant');
+accounting.printEmployeeInfo();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
-const add = (...numbers: number[]) => {
-    return numbers.reduce((total, currentVal) => {
-        return total + currentVal;
-    }, 0);
+class Department {
+    name: string;
+    private employees: string[] = [];
+
+    constructor(n: string) {
+        this.name = n;
+    }
+    describe(this: Department) {
+        console.log('This is the department of ', this.name)
+    }
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfo(this: Department) {
+        console.log('employee length ', this.employees.length);
+        console.log(this.employees);
+    }
 }
 
-console.log(add(1,2,3,4,5,6));
+const accounting = new Department('accounting');
+accounting.addEmployee('tony stark');
+accounting.addEmployee('Jeff Cant')
+accounting.printEmployeeInfo();
