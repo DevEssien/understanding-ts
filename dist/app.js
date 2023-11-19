b4f97db6abf6d777c2e5ff0a0ad92a981c3f1f1c
essien emmanuel <essienemma300@gmail.com>
2023-11-19 18:48:34 +0100

    getters and setters method

--- dist/app.js
+++ dist/app.js
@@ -6,44 +6,56 @@ class Department {
         this.name = n;
     }
     describe() {
-        console.log('This is the department of ', this.name);
+        console.log("This is the department of ", this.name);
     }
     addEmployee(employee) {
         this.employees.push(employee);
     }
     printEmployeeInfo() {
-        console.log('employee length ', this.employees.length);
+        console.log("employee length ", this.employees.length);
         console.log(this.employees);
     }
 }
 class ITDepartment extends Department {
     constructor(id, admins = []) {
-        super(id, 'IT');
+        super(id, "IT");
         this.admins = admins;
     }
 }
 class AccountingDepartment extends Department {
+    get mostRecentReport() {
+        if (this.lastReport)
+            return this.lastReport;
+        throw new Error("No report found");
+    }
+    set mostRecentReport(value) {
+        if (!value)
+            throw new Error('please pass in a valid value');
+        this.addReport(value);
+    }
     constructor(id, reports = []) {
-        super(id, 'Accounting');
+        super(id, "Accounting");
         this.reports = reports;
+        this.lastReport = reports[0];
     }
     addEmployee(name) {
-        if (name.toLowerCase() === 'max')
+        if (name.toLowerCase() === "max")
             return;
         else
             this.employees.push(name);
     }
     addReport(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     printReport() {
         console.log(this.reports);
     }
 }
-const Accounting = new AccountingDepartment('63ah4nbc93d3rm');
-Accounting.addEmployee('Max');
-Accounting.addEmployee('Jeff Cant');
-Accounting.addReport('this is another report from the bad man pablo gang');
-console.log('Accounting ', Accounting);
-Accounting.printReport();
+const accounting = new AccountingDepartment("63ah4nbc93d3rm");
+accounting.mostRecentReport = 'we are still working on the end of years report from the department of accounting';
+accounting.addReport("this is another report from the bad man pablo gang");
+console.log(accounting.mostRecentReport);
+console.log("accounting ", accounting);
+accounting.printReport();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,67 @@
 class Department {
-    private readonly id: string;
-    name: string;
-    protected employees: string[] = [];
+  private readonly id: string;
+  name: string;
+  protected employees: string[] = [];
 
-    constructor(id: string,  n: string) {
-        this.id = id;
-        this.name = n;
-    }
-    describe(this: Department) {
-        console.log('This is the department of ', this.name)
-    }
-    addEmployee(employee: string) {
-        this.employees.push(employee);
-    }
-    printEmployeeInfo(this: Department) {
-        console.log('employee length ', this.employees.length);
-        console.log(this.employees);
-    }
+  constructor(id: string, n: string) {
+    this.id = id;
+    this.name = n;
+  }
+  describe(this: Department) {
+    console.log("This is the department of ", this.name);
+  }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInfo(this: Department) {
+    console.log("employee length ", this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 class ITDepartment extends Department {
-    constructor(id: string, public admins: string[] = []) {
-        super(id, 'IT');
-    }
+  constructor(id: string, public admins: string[] = []) {
+    super(id, "IT");
+  }
 }
 
 class AccountingDepartment extends Department {
-    constructor(id: string, private reports: string[] = []) {
-        super(id, 'Accounting');
-    }
-    addEmployee(name: string) {
-        if (name.toLowerCase() === 'max') return;
-        else this.employees.push(name);
-    }
-    addReport(this: AccountingDepartment, text: string) {
-        this.reports.push(text);
-    }
-    printReport(this: AccountingDepartment) {
-        console.log(this.reports);
-    }
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) return this.lastReport;
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) throw new Error('please pass in a valid value');
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[] = []) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+  addEmployee(name: string) {
+    if (name.toLowerCase() === "max") return;
+    else this.employees.push(name);
+  }
+  addReport(this: AccountingDepartment, text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+  printReport(this: AccountingDepartment) {
+    console.log(this.reports);
+  }
 }
 
-const Accounting = new AccountingDepartment('63ah4nbc93d3rm');
-Accounting.addEmployee('Max');
-Accounting.addEmployee('Jeff Cant')
+// Accounting.addEmployee('Max');
+// Accounting.addEmployee('Jeff Cant')
 // Accounting.printEmployeeInfo();
-Accounting.addReport('this is another report from the bad man pablo gang');
-console.log('Accounting ', Accounting);
-Accounting.printReport()
+
+const accounting = new AccountingDepartment("63ah4nbc93d3rm");
+accounting.mostRecentReport = 'we are still working on the end of years report from the department of accounting';  //we execute the set method as a property and not as a method
+accounting.addReport("this is another report from the bad man pablo gang");
+console.log(accounting.mostRecentReport);
+console.log("accounting ", accounting);
+accounting.printReport();
