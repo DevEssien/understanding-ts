d6e2fdf8d46e95885976143a7489d588cecc39c5
essien emmanuel <essienemma300@gmail.com>
2023-11-22 14:37:45 +0100

    index properties

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,9 @@
 "use strict";
 const inputElement = document.querySelector('input');
 inputElement.value = 'Hi there';
+const errorBag = {
+    email: 'No email provided',
+    username: 'Username must start with a character',
+    nationality: 'Nigeria'
+};
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,13 @@
 const inputElement = <HTMLInputElement>document.querySelector('input')!;
 
 inputElement.value = 'Hi there';
+
+interface ErrorContainer {
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email: 'No email provided',
+    username: 'Username must start with a character',
+    nationality: 'Nigeria'
+}
