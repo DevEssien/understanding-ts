a7acf96597ad8e69803c120bbf2c5608c2411f02
essien emmanuel <essienemma300@gmail.com>
2023-11-22 19:45:46 +0100

    Generics with classes

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,26 @@
 "use strict";
-const extractAndConvert = (obj, key) => {
-    return 'value ' + obj[key];
-};
-extractAndConvert({ name: 'Max' }, 'name');
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.indexOf(item) === -1)
+            return;
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const stringStorage = new DataStorage();
+stringStorage.addItem('Max');
+stringStorage.addItem('Manu');
+stringStorage.removeItem('Manu');
+console.log(stringStorage.getItems());
+const numberStorage = new DataStorage();
+numberStorage.addItem(2);
+console.log(numberStorage.getItems());
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -27,10 +27,38 @@
 // };
 // console.log(countAndDescribe(["clear", "copy", "insert"]));
 
-const extractAndConvert = <T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) => {
-    return 'value ' + obj[key];
-};
-extractAndConvert({name: 'Max'}, 'name');
+// const extractAndConvert = <T extends object, U extends keyof T>(
+//   obj: T,
+//   key: U
+// ) => {
+//     return 'value ' + obj[key];
+// };
+// extractAndConvert({name: 'Max'}, 'name');
+
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) return
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [ ...this.data]
+    }
+}
+
+const stringStorage = new DataStorage<string>();
+stringStorage.addItem('Max');
+stringStorage.addItem('Manu');
+stringStorage.removeItem('Manu');
+console.log(stringStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(2);
+console.log(numberStorage.getItems());
