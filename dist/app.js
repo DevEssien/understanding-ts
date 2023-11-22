a2e3221d6250860f2817e4a5348141cca6171e6c
essien emmanuel <essienemma300@gmail.com>
2023-11-22 19:24:56 +0100

    Generics with constraints

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,6 @@
 "use strict";
-const mergeObject = (obj1, obj2) => {
-    return Object.assign(Object.assign({}, obj1), obj2);
+const extractAndConvert = (obj, key) => {
+    return 'value ' + obj[key];
 };
-const obj = mergeObject({ a: 2, b: 3 }, { c: 3, d: 4 });
-console.log(obj.c);
+extractAndConvert({ name: 'Max' }, 'name');
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,28 @@
 //     data.toUpperCase()
 // })
 
-const mergeObject = <T extends object, U extends object>( obj1: T, obj2: U ) => {
-    return { ...obj1, ...obj2 }
-}
-const obj = mergeObject({a: 2, b: 3}, {c: 3, d: 4});
-console.log(obj.c)
+// const mergeObject = <T extends object, U extends object>( obj1: T, obj2: U ) => {
+//     return { ...obj1, ...obj2 }
+// }
+// const obj = mergeObject({a: 2, b: 3}, {c: 3, d: 4});
+// console.log(obj.c)
+
+// interface Lengthy {
+//   length: number;
+// }
+
+// const countAndDescribe = <T extends Lengthy>(element: T): [T, string] => {
+//   let description = "Got no value";
+//   if (element.length === 1) description = "Got 1 element";
+//   else if (element.length > 1) description = `Got ${element.length} elements`;
+//   return [element, description];
+// };
+// console.log(countAndDescribe(["clear", "copy", "insert"]));
+
+const extractAndConvert = <T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) => {
+    return 'value ' + obj[key];
+};
+extractAndConvert({name: 'Max'}, 'name');
