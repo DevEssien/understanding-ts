0fcb4883aa7e4c28abcfba1043532637ffcfc20b
essien emmanuel <essienemma300@gmail.com>
2023-11-23 20:20:24 +0100

    first class decorators

--- dist/app.js
+++ dist/app.js
@@ -1,26 +1,21 @@
 "use strict";
-class DataStorage {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+const Logger = (target) => {
+    console.log(target);
+};
+let Person = class Person {
     constructor() {
-        this.data = [];
+        this.name = 'Max';
+        console.log('logging the person...');
     }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1)
-            return;
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-    getItems() {
-        return [...this.data];
-    }
-}
-const stringStorage = new DataStorage();
-stringStorage.addItem('Max');
-stringStorage.addItem('Manu');
-stringStorage.removeItem('Manu');
-console.log(stringStorage.getItems());
-const numberStorage = new DataStorage();
-numberStorage.addItem(2);
-console.log(numberStorage.getItems());
+};
+Person = __decorate([
+    Logger
+], Person);
+const person = new Person();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,14 @@
-// const list: Array<string> = []
+const Logger = (target: Function) => {
+    console.log(target)
+}
 
-// const promise: Promise<string>  = new Promise((resolve, reject) => {
-//     const r = 'red'
-//     if (r === 'red') resolve('red');
-// });
+@Logger
+class Person {
+    name = 'Max';
 
-// promise.then(data => {
-//     data.toUpperCase()
-// })
-
-// const mergeObject = <T extends object, U extends object>( obj1: T, obj2: U ) => {
-//     return { ...obj1, ...obj2 }
-// }
-// const obj = mergeObject({a: 2, b: 3}, {c: 3, d: 4});
-// console.log(obj.c)
-
-// interface Lengthy {
-//   length: number;
-// }
-
-// const countAndDescribe = <T extends Lengthy>(element: T): [T, string] => {
-//   let description = "Got no value";
-//   if (element.length === 1) description = "Got 1 element";
-//   else if (element.length > 1) description = `Got ${element.length} elements`;
-//   return [element, description];
-// };
-// console.log(countAndDescribe(["clear", "copy", "insert"]));
-
-// const extractAndConvert = <T extends object, U extends keyof T>(
-//   obj: T,
-//   key: U
-// ) => {
-//     return 'value ' + obj[key];
-// };
-// extractAndConvert({name: 'Max'}, 'name');
-
-
-class DataStorage<T extends string | number | boolean> {
-    private data: T[] = [];
-
-    addItem(item: T) {
-        this.data.push(item);
-    }
-
-    removeItem(item: T) {
-        if (this.data.indexOf(item) === -1) return
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-
-    getItems() {
-        return [ ...this.data]
+    constructor() {
+        console.log('logging the person...')
     }
 }
 
-const stringStorage = new DataStorage<string>();
-stringStorage.addItem('Max');
-stringStorage.addItem('Manu');
-stringStorage.removeItem('Manu');
-console.log(stringStorage.getItems());
-
-const numberStorage = new DataStorage<number>();
-numberStorage.addItem(2);
-console.log(numberStorage.getItems());
+const person = new Person()
